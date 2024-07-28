174930df8820cac9eed51f481ebb952c5182dfb6
Danial Zahid <iamdanialzahid@gmail.com>
2024-07-28 19:09:40 +0500

    Move `NavigationDesktop` into `Components` folder.

---
+++ src/Components/Navigation/NavigationDesktop.tsx
@@ -0,0 +1,46 @@
+const navItems = [
+  {
+    name: 'Home',
+    link: '/#hero',
+  },
+
+  {
+    name: 'About Us',
+    link: '/#about-us',
+  },
+
+  {
+    name: 'Product',
+    link: '/#product',
+  },
+
+  {
+    name: 'Company',
+    link: '/#company',
+  },
+
+  {
+    name: 'Contact Us',
+    link: '/#contact-us',
+  },
+];
+
+export function NavigationDesktop() {
+  const item = navItems.map((item) => (
+    <li key={item.name}>
+      <a
+        href={item.link}
+        title={item.name}
+        className='decoration-blue-500 hover:underline hover:underline-offset-8'
+      >
+        {item.name}
+      </a>
+    </li>
+  ));
+
+  return (
+    <nav>
+      <ul className='flex items-center gap-10 font-semibold'>{item}</ul>
+    </nav>
+  );
+}
