7f66f05e490c01e1b8551600e084a64e923f698e
Danial Zahid <iamdanialzahid@gmail.com>
2024-07-28 20:46:05 +0500

    Update classes and content.

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,17 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Hero() {
   return (
-    <section className='grid gap-16' id='hero'>
-      <div className='grid gap-8'>
-        <h1>
+    <section className='grid gap-16 text-center' id='hero'>
+      <div className='grid gap-10'>
+        <h1 className='text-6xl font-bold'>
           Increase Productivity with
           <br />
           Advanced HR Management
         </h1>
 
-        <p className='text-center'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione iste
-          minima vel eveniet corrupti fuga deserunt totam atque voluptatibus
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <br />
+          Ratione iste minima vel eveniet corrupti fuga deserunt totam atque
+          voluptatibus
         </p>
 
         <div className='flex flex-wrap items-center justify-center gap-2'>
@@ -27,7 +29,7 @@ export default function Hero() {
       </div>
 
       <img
-        className='justify-self-center'
+        className='justify-self-center shadow-2xl shadow-blue-600/40'
         width={900}
         src='/assets/img/hero.jpg'
         alt=''
