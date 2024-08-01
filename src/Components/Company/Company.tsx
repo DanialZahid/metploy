17e621ae1c39af5f7a2f89894be502e4914644d5
Danial Zahid <iamdanialzahid@gmail.com>
2024-08-02 00:25:11 +0500

    Add more content to the Company section.

--- src/Components/Company/Company.tsx
+++ src/Components/Company/Company.tsx
@@ -1,7 +1,7 @@
 export default function Company() {
   return (
-    <section className='grid gap-20 text-center' id='company'>
-      <div className='grid gap-10'>
+    <section className='grid gap-20' id='company'>
+      <div className='grid gap-10 text-center'>
         <h2>
           Manage an Efficient Schedule System
           <br />
@@ -26,6 +26,37 @@ export default function Company() {
         title='YouTube Video'
         allowFullScreen
       ></iframe>
+
+      <section className='grid grid-cols-2 gap-16 p-0'>
+        <div className='grid gap-10'>
+          <h2>
+            2700+ Customers Shared
+            <br />
+            their Love for Metploy
+          </h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe est
+            cupiditate officiis dignissimos exercitationem repellat!
+          </p>
+        </div>
+
+        <div className='grid min-h-[600px] items-end rounded-xl bg-[url(/assets/img/hero.jpg)] p-8'>
+          <div className='self-e grid gap-10 rounded-xl bg-black/50 p-8 backdrop-blur-sm'>
+            <p className='text-xl italic text-white'>
+              &quot;Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+              libero. Laborum aliquam recusandae dolores placeat rerum eos ullam
+              minus numquam!&quot;
+            </p>
+
+            <div className='grid gap-2'>
+              <span className='text-xl font-bold'>John Doe</span>
+              <span className='text-gray-300'>
+                Head of Human Resources at ABC Company
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
   );
 }
