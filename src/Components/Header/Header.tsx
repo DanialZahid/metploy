e591f41f1f816362902870636b6b1d8563a99e23
Danial Zahid <iamdanialzahid@gmail.com>
2024-08-02 21:22:31 +0500

    Make Header sticky.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,14 +5,16 @@ import Logo from './Logo';
 
 export default function Header() {
   return (
-    <header className='m-auto grid max-w-7xl grid-cols-[1fr_auto_1fr] items-center gap-4 px-4 py-8'>
-      <Logo />
+    <header className='sticky top-0 z-10 bg-black shadow-md'>
+      <div className='m-auto grid max-w-7xl grid-cols-[1fr_auto_1fr] items-center gap-4 px-4 py-8'>
+        <Logo />
 
-      <NavigationDesktop />
+        <NavigationDesktop />
 
-      <div className='flex gap-2 justify-self-end'>
-        <ButtonTransparent content='Sign Up' />
-        <ButtonBlue content='Log In' />
+        <div className='flex gap-2 justify-self-end'>
+          <ButtonTransparent content='Sign Up' />
+          <ButtonBlue content='Log In' />
+        </div>
       </div>
     </header>
   );
