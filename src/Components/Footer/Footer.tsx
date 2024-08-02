068a3d8facaca13cb290e4b8f2f6c5ed62c75e3a
Danial Zahid <iamdanialzahid@gmail.com>
2024-08-02 22:32:15 +0500

    Update Footer.

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,3 +1,11 @@
+import Logo from '../Header/Logo';
+
 export default function Footer() {
-  return <footer className='m-auto max-w-screen-xl'></footer>;
+  return (
+    <footer className='border-t-2 border-neutral-900 bg-neutral-950'>
+      <div className='m-auto max-w-screen-xl px-4 py-12'>
+        <Logo />
+      </div>
+    </footer>
+  );
 }
