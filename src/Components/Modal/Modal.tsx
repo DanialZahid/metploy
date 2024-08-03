1ed6d366277647a2ebff94107d4752afc96dca34
Danial Zahid <iamdanialzahid@gmail.com>
2024-08-03 21:52:13 +0500

    Add Framer Motion animation to Modal.

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import { createPortal } from 'react-dom';
 import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface ModalProps {
   showModal: boolean;
@@ -32,30 +33,41 @@ export default function Modal({
     };
   }, [showModal, setShowModal]);
 
-  return (
-    <>
-      {showModal &&
-        createPortal(
-          <div className='fixed top-0 z-10 flex h-full w-full items-center justify-center bg-black/75 backdrop-blur-sm'>
-            <div className='grid content-start gap-8 overflow-auto rounded-2xl border-2 border-neutral-900 bg-neutral-950 p-8 shadow-lg lg:max-h-screen lg:w-[900px]'>
-              <div className='flex items-center justify-between gap-8'>
-                <h2>{title}</h2>
+  return createPortal(
+    <AnimatePresence>
+      {showModal && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.15 }}
+          className='fixed top-0 z-10 flex h-full w-full items-center justify-center bg-black/75 backdrop-blur-sm'
+        >
+          <motion.div
+            initial={{ scale: 0.95 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.95 }}
+            transition={{ duration: 0.3, type: 'spring' }}
+            className='grid content-start gap-8 overflow-auto rounded-2xl border-2 border-neutral-900 bg-neutral-950 p-8 shadow-lg lg:max-h-screen lg:w-[900px]'
+          >
+            <div className='flex items-center justify-between gap-8'>
+              <h2>{title}</h2>
 
-                <FontAwesomeIcon
-                  icon={faXmark}
-                  className='h-5 w-5 cursor-pointer rounded-full border-2 border-neutral-800 bg-neutral-900 p-2 transition-colors hover:border-red-800 hover:bg-red-800'
-                  onClick={() => {
-                    setShowModal(false);
-                  }}
-                  title='Close Modal'
-                />
-              </div>
-
-              {children}
+              <FontAwesomeIcon
+                icon={faXmark}
+                className='h-5 w-5 cursor-pointer rounded-full border-2 border-neutral-800 bg-neutral-900 p-2 transition-colors hover:border-red-800 hover:bg-red-800'
+                onClick={() => {
+                  setShowModal(false);
+                }}
+                title='Close Modal'
+              />
             </div>
-          </div>,
-          document.body,
-        )}
-    </>
+
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>,
+    document.body,
   );
 }
